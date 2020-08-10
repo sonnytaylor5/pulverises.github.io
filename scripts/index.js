c58702e18e2ff1510d57564948ebc63b1e266ce8
sonnytaylor <sonnytaylor5@gmail.com>
2020-08-10 21:51:41 +0100

    split components out

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    setTimeout(function(){
+        $('.lds').fadeOut("fast");
+    }, 4500);
+
+    $('.sidebar__openbutton').click(function () {
+        if ($('#sidebar__btn').hasClass("fa-arrow-right")) {
+            $(".container").animate({ "right": "-=170px" }, 1000, function () { });
+            $(".sidebar").animate({ "right": "-=340px" }, 1000, function () {
+                $('.sidebar__btn').removeClass("fa-arrow-right").addClass("fa-arrow-left");
+            });
+        } else {
+            $(".container").animate({ "right": "+=170px" }, 1000, function () { });
+            $(".sidebar").animate({ "right": "+=340px" }, 1000, function () {
+                $('.sidebar__btn').removeClass("fa-arrow-left").addClass("fa-arrow-right");
+            });
+        }
+    });
+
+    window.setInterval(function () {
+        $('.sidebar__btncontainer').animate({ "left": "-=38px" }, 1000, function () {
+            $('.sidebar__btncontainer').animate({ "left": "-=38px" }, 1000, function () {
+                $('.sidebar__btncontainer').animate({ "left": "-=38px" }, 1000, function () {
+                    $('.sidebar__btncontainer').css("left", "6px");
+                });
+            });
+        });
+    }, 3000);
+});
