dd23beff2e1a9fbc31ff573e49e014766befc859
sonnytaylor <sonnytaylor5@gmail.com>
2021-04-24 14:09:56 +0100

    raids sign up

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+var errors;
 $(document).ready(function () {
     setTimeout(function () {
         $('.loading-spinner-container').fadeOut("fast");
@@ -8,7 +9,7 @@ $(document).ready(function () {
         if ($('#sidebar__btn').hasClass("fa-arrow-right")) {
             $(".container").animate({ "right": "-=170px" }, 1000, function () { });
             $(".sidebar").animate({ "right": "-=340px" }, 1000, function () {
-                $('.sidebar__btn').removeClass("fa-arrow-right").addClass("fa-arrow-left"); 
+                $('.sidebar__btn').removeClass("fa-arrow-right").addClass("fa-arrow-left");
             });
         } else {
             $(".container").animate({ "right": "+=170px" }, 1000, function () { });
@@ -59,7 +60,7 @@ $(document).ready(function () {
         });
     }, 3000);
 
-    $('#form__idea').submit(function(e){
+    $('#form__idea').submit(function (e) {
         e.preventDefault();
         var data = new FormData($('#form__idea')[0]);
         $.ajax({
@@ -71,15 +72,62 @@ $(document).ready(function () {
             contentType: false,
             cache: false
         })
-        .done(function (data) {
-            if(data.success){
-                hideModal();
-                $('#form__file-label').text("");
-                document.getElementById("container").src = "";
-                $('#form__idea')[0].reset();
-                alert("Your message has been sent.");
-            }           
-        });
+            .done(function (data) {
+                if (data.success) {
+                    hideModal();
+                    $('#form__file-label').text("");
+                    document.getElementById("container").src = "";
+                    $('#form__idea')[0].reset();
+                    alert("Your message has been sent.");
+                }
+            });
+    });
+
+    $('#form__raid').submit(function (e) {
+        e.preventDefault();
+        var ids = ["osrsname", "discordname", "experienceid", "kc", "gearid", "joinid"];
+            for (var i = 0; i < ids.length; i++) {
+                if($("#" + ids[i]).prev().hasClass("pulv-error-message")){
+                    $("#" + ids[i]).prev().remove()
+                    if($("#" + ids[i]).is("input")){
+                        $("#" + ids[i]).removeClass("pulv-input--error");
+                        $("#" + ids[i]).parent().removeClass("pulv-form-group--error");
+                    }else{
+                        $("#" + ids[i]).parent().parent().removeClass("pulv-form-group--error");
+                    }
+                }
+            }
+
+        var data = new FormData($('#form__raid')[0]);
+        $.ajax({
+            type: 'POST',
+            url: 'https://sonnerrs-bot.herokuapp.com/raid/',
+            enctype: 'multipart/form-data',
+            data: data,
+            processData: false,
+            contentType: false,
+            cache: false
+        })
+            .done(function (data) {
+                console.log(data);
+                if (data.success) {
+                    $('#form__raid')[0].reset();
+                    alert("Your message has been sent.");
+                } else {
+                    if (data && data.errors) {
+                        for (var i = 0; i < data.errors.length; i++) {
+                            $('<span id="osrsname-error" class="pulv-error-message"><span class="pulv-visually-hidden">Error:</span> ' + data.errors[i].message + '</span>').insertBefore("#" + data.errors[i].formid);
+                            if($("#" + data.errors[i].formid).is("input")){
+                                $("#" + data.errors[i].formid).addClass("pulv-input--error");
+                                $("#" + data.errors[i].formid).parent().addClass("pulv-form-group--error");
+                            }else{
+                                $("#" + data.errors[i].formid).parent().parent().addClass("pulv-form-group--error");
+                            }
+                        }
+                        $("#" + data.errors[0].formid).parent().attr("tabindex", -1).focus();
+                    }
+                }
+            });
     });
 
     const fileInput = document.getElementById("fileinput");
@@ -88,23 +136,23 @@ $(document).ready(function () {
         fileInput.files = e.clipboardData.files;
     });
 
-    document.onpaste = function(pasteEvent) {
+    document.onpaste = function (pasteEvent) {
         // consider the first item (can be easily extended for multiple items)
         var item = pasteEvent.clipboardData.items[0];
-      
+
         if (item.type.indexOf("image") === 0) {
-          var blob = item.getAsFile();
-      
-          var reader = new FileReader();
-          reader.onload = function(event) {
-            document.getElementById("container").src = event.target.result;
-          };
-          try{
-          reader.readAsDataURL(blob);
-          $('#form__file-label').text("Pasted image from clipboard");
-          }catch(e){
-              alert("There was an issue reading your clipboard data.");
-          }
+            var blob = item.getAsFile();
+
+            var reader = new FileReader();
+            reader.onload = function (event) {
+                document.getElementById("container").src = event.target.result;
+            };
+            try {
+                reader.readAsDataURL(blob);
+                $('#form__file-label').text("Pasted image from clipboard");
+            } catch (e) {
+                alert("There was an issue reading your clipboard data.");
+            }
         }
-      }
+    }
 });
