9ff72a40435882be85d5a3c844eb0f80fdf43bab
sonnytaylor <sonnytaylor5@gmail.com>
2021-05-25 20:33:51 +0100

    new js

--- scripts/bingo.js
+++ scripts/bingo.js
@@ -2,6 +2,8 @@ var res;
 var activeTeam;
 var socket;
 
+var pointArr = [0, 50, 50, 90, 60, 100, 50, 60, 70, 90, 70, 100, 120, 150, 130, 100, 60, 110, 60, 80, 40, 70, 70, 70, 100, 50];
+
 function addBorders(teamNumber){  
     activeTeam = teamNumber;
     $('.tile').removeClass("achieved");
@@ -30,16 +32,33 @@ $(document).ready(function(){
             addBorders(socketRes.teamNumber);
         }
 
+        for(var i = 0; i < res.teams.length; i++){
+            var points = 0;
+            for(var j = 0; j < res.teams[i].completedTiles.length; j++)
+            {
+                points += pointArr[res.teams[i].completedTiles[j]];
+            }
+            res.teams[i].points = points;
+        }
+
         $('.counter').remove();
         for(var i = 0; i < res.teams.length; i++){
-            $('#team' + res.teams[i].teamNumber).append("<span class='counter'>" + res.teams[i].completedTiles.length +"/25</span>");
+            $('#team' + res.teams[i].teamNumber).append("<span class='counter'>" + res.teams[i].completedTiles.length +"/2000</span>");
         }
     });
     $.ajax({url: "https://sonnerrs-bot.herokuapp.com/team/", success: function(result){
         res = result;
+        for(var i = 0; i < res.teams.length; i++){
+            var points = 0;
+            for(var j = 0; j < res.teams[i].completedTiles.length; j++)
+            {
+                points += pointArr[res.teams[i].completedTiles[j]];
+            }
+            res.teams[i].points = points;
+        }
 
         for(var i = 0; i < res.teams.length; i++){
-            $('#team' + res.teams[i].teamNumber).append("<span class='counter'>" + res.teams[i].completedTiles.length +"/25</span>");
+            $('#team' + res.teams[i].teamNumber).append("<span class='counter'>" + res.teams[i].points +"/2000</span>");
         }
         
       },error:function(result){
